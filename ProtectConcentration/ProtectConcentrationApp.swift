457debf3d58da82af1b8993a1ec98dce4e087811
RyotaLab <orengi12260@gmail.com>
2024-10-19 13:14:31 +0900

    Initial Commit

---
+++ ProtectConcentration/ProtectConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtectConcentrationApp.swift
+//  ProtectConcentration
+//
+//  Created by 渡邊涼太 on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct ProtectConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
